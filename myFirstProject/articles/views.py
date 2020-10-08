5c55014b39dd0723b05b30284f17ecda1e31ca8a
saadhassan999 <saadhassan99950@gmail.com>
2020-10-08 12:58:34 +0500

    init

---
+++ myFirstProject/articles/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def articles_list(request):
+    return render(request, 'articles/articles_list.html')
---
+++ myFirstProject/myFirstProject/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def home_view(request):
+    return render(request, 'homepage.html')
+    # return HttpResponse('<h1>This is Homepage</h1>')
+
+def about_view(request):
+    return render(request, 'about.html')
+    # return HttpResponse('<h1>This is about page</h1>')
