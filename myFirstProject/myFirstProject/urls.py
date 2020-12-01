f52b4482f52174960c525d20eba4c63f30da67b8
saadhassan999 <saadhassan99950@gmail.com>
2020-12-01 23:02:11 +0500

    new commit

--- myFirstProject/myFirstProject/urls.py
+++ myFirstProject/myFirstProject/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('articles/', include('articles.urls')),
     path('', views.home_view),
     path('about/', views.about_view),
-    
 ]
+urlpatterns += staticfiles_urlpatterns()
