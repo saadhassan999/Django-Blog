5c62fb26350aeca48cd6a601ad5a805aac2547ac
saadhassan999 <saadhassan99950@gmail.com>
2020-12-03 00:42:38 +0500

    new changes

--- myFirstProject/articles/views.py
+++ myFirstProject/articles/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from .models import Article
+from django.http import HttpResponse
 # Create your views here.
 
 def articles_list(request):
     articles = Article.objects.all().order_by('date')
     return render(request, 'articles/articles_list.html', {'articles':articles})
+def articles_detail(request, slug):
+    return HttpResponse('This is Slug : ' + slug)
