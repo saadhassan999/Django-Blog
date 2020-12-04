c1522ae3d2f1edc5640ee3a2f232076143ba1069
saadhassan999 <saadhassan99950@gmail.com>
2020-12-04 20:18:29 +0500

    login_functionality

--- myFirstProject/articles/views.py
+++ myFirstProject/articles/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
 from .models import Article
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 def articles_list(request):
     articles = Article.objects.all().order_by('date')
     return render(request, 'articles/articles_list.html', {'articles':articles})
+
 def articles_detail(request, slug):
     # return HttpResponse('This is Slug : ' + slug)
     article = Article.objects.get(slug=slug)
     return render(request, 'articles/articles_detail.html', {'article' : article})
 
+@login_required(login_url='/accounts/login')
+def create_article(request):
+    return render(request, 'articles/create_articles.html')
--- myFirstProject/myFirstProject/urls.py
+++ myFirstProject/myFirstProject/urls.py
@@ -7,6 +7,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('articles/', include('articles.urls')),
+    path('accounts/', include('accounts.urls')),
     path('', views.home_view),
     path('about/', views.about_view),
 ]
