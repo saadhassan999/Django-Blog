78140613b2c6edf91e1c28a1a6ea2e4d8ace6deb
saadhassan999 <saadhassan99950@gmail.com>
2020-12-07 21:06:52 +0500

    Redirecting the Homepage

--- myFirstProject/myFirstProject/urls.py
+++ myFirstProject/myFirstProject/urls.py
@@ -4,11 +4,12 @@ from . import views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf import settings
 from django.conf.urls.static import static
+from articles import views as vw
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('articles/', include('articles.urls')),
     path('accounts/', include('accounts.urls')),
-    path('', views.home_view),
+    path('', vw.articles_list, name='home'),
     path('about/', views.about_view),
 ]
 urlpatterns += staticfiles_urlpatterns()
--- myFirstProject/myFirstProject/views.py
+++ myFirstProject/myFirstProject/views.py
@@ -1,9 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-def home_view(request):
-    return render(request, 'homepage.html')
-    # return HttpResponse('<h1>This is Homepage</h1>')
+# def home_view(request):
+#     return render(request, 'homepage.html')
+#     # return HttpResponse('<h1>This is Homepage</h1>')
 
 def about_view(request):
     return render(request, 'about.html')
